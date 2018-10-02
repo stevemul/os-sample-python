f17cbf826166e68d2b5fa1116a3ff5eb8a14001b
Steve Mulholland <stevemul@users.noreply.github.com>
2018-10-02 16:45:58 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ def hello():
 
 @application.route("/demo")
 def hellodemo():
-    return "<b>Hello World!</b>"
+    return "<b>Hello World!</b><br><img src=\"https://media.giphy.com/media/QgixZj4y3TwnS/giphy.gif\">"
 
 if __name__ == "__main__":
     application.run()
